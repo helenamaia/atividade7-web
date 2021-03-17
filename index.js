5de2f81ca182f04c455e4bdb6d224f0e96ac31ac
helena karol <helenadiasmaia@gmail.com>
2021-03-17 14:11:34 -0300

    video aula 4

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ console.log(idade);
 idade = idade + 15; 
 
 console.log(idade);
+
+const peso = prompt('informe o peso:')
+console.log('peso do usuario: '+peso);
+
+const apagar = confirm('Voce de deseja apagar?');
+console.log(apagar);
