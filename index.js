86de134a1503b6f05e5c24e0458c15d739b6b6ea
helena karol <helenadiasmaia@gmail.com>
2021-03-17 14:03:47 -0300

    video aula 3

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//alert('Mensagem do arquivo index.js');
+const nome = 'Helena';
+
+console.log(nome);
+
+
+
+let idade; 
+
+idade = 22;
+
+console.log(idade);
+
+idade = idade + 15; 
+
+console.log(idade);
